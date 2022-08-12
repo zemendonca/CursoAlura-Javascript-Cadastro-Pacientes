9e5e1d4a630536f8e69337f9eefeb28a4d54f235
José Mendonça <108704192+zemendonca@users.noreply.github.com>
2022-08-11 23:43:47 -0300

    alura javascript course completed
    Patient Registration System

---
+++ js/calcula-imc.js
@@ -0,0 +1,58 @@
+var titulo = document.querySelector('.titulo')
+titulo.textContent = 'Aparecida Nutricionista'
+
+var pacientes = document.querySelectorAll('.paciente')
+
+for (var i = 0; i < pacientes.length; i++) {
+  var paciente = pacientes[i]
+  var tdPeso = paciente.querySelector('.info-peso')
+  var peso = tdPeso.textContent
+  var tdAltura = paciente.querySelector('.info-altura')
+  var altura = tdAltura.textContent
+
+  var tdImc = paciente.querySelector('.info-imc')
+
+  var pesoEValido = validaPeso(peso)
+  var alturaEValida = validaAltura(altura)
+
+  if (!pesoEValido) {
+    console.log('Peso Inválido')
+    pesoEValido = false
+    tdImc.textContent = 'Peso Inválido'
+    paciente.classList.add('paciente-invalido')
+  }
+
+  if (!alturaEValida) {
+    console.log('Altura Inválida')
+    alturaEValida = false
+    tdImc.textContent = 'Altura Inválida'
+    paciente.classList.add('paciente-invalido')
+  }
+
+  if (alturaEValida && pesoEValido) {
+    var imc = calculaImc(peso, altura)
+    tdImc.textContent = imc
+  }
+}
+
+function validaPeso(peso) {
+  if (peso >= 0 && peso < 1000) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function validaAltura(altura) {
+  if (altura >= 0 && altura <= 3) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function calculaImc(peso, altura) {
+  var imc = 0
+  imc = peso / (altura * altura)
+  return imc.toFixed(2)
+}
